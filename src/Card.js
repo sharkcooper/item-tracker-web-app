1e682fc05ad30b4e17606cf30053ff9946ed0071
Neema Nejad <dnnejad@gmail.com>
2022-04-17 11:38:13 -0700

    removed all files from dist directory, added basic visuals for cards with sample title, availability, and price, also added an expand/shrink button that expands or shrinks each individual card

--- src/Card.js
+++ src/Card.js
@@ -2,10 +2,66 @@ import React from 'react';
 import './Card.css'
 
 class Card extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            expanded: false,
+        }
+    }
+
+    toggleExpand() {
+        this.setState({
+            expanded: !this.state.expanded,
+        })
+        console.log("expanded state is now:")
+        console.log(this.state.expanded)
+    }
+
+    createTitle() {
+        return (
+            <div className='titleContainer'>
+                <p className='cardTitle leftCardItem'>Sample Title</p>
+                {
+                    this.state.expanded ? 
+                        <button className='expandButton rightCardItem' onClick={() => this.toggleExpand()}>-</button> :
+                        <button className='expandButton rightCardItem' onClick={() => this.toggleExpand()}>+</button>
+                }
+            </div>
+        )
+    }
+
+    createDetailsArea() {
+        if (this.state.expanded){
+            return (
+                <div className='detailsContainer'>
+                    {this.createAvailability()}
+                    {this.createPrice()}
+                </div>
+            )
+        }
+    }
+
+    createAvailability() {
+        return (
+            <div className='availabilityContainer statusContainer leftCardItem'>
+                <p className='availability'>Sample Availability</p>
+            </div>
+        )
+    }
+
+    createPrice() {
+        return (
+            <div className='priceContainer statusContainer rightCardItem'>
+                <p className='price'>Sample Price</p>
+            </div>
+        )
+    }
+
     render() {
         return (
             <div className='card'>
-
+                {this.createTitle()}
+                {this.createDetailsArea()}
             </div>
         )
     }
